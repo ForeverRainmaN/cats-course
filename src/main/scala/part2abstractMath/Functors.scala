a3abdb71edab31f8a20b3af5b3d423abf4945d0e
Курчин Иоанн Вячеславович <lvrainman@gmail.com>
2025-09-07 21:34:00 +0300

    Functors recap

--- src/main/scala/part2abstractMath/Functors.scala
+++ src/main/scala/part2abstractMath/Functors.scala
@@ -5,7 +5,7 @@ import scala.util.Try
 object Functors {
 
   val aModifiedList = List(1, 2, 3).map(_ + 1) // List(2,3,4)
-  val aModifiedOption = Option(2).map(_ + 1) // Some(3)
+  val aModifiedOption = Option(2).map(_ + 1) // Some(2)
   val aModifiedTry = Try(42).map(_ + 1) // Success(43)
 
   // simplified definition
@@ -14,69 +14,63 @@ object Functors {
   }
 
   import cats.Functor
-  import cats.instances.list.*
+  import cats.instances.list.* // includes Functor[List]
 
   val listFunctor = Functor[List]
-  val incrementedNumbers = listFunctor.map(List(1, 2, 3))(_ + 1) // List(2,3,4)
+  val incrementedNumbers = listFunctor.map(List(1, 2, 3))(_ + 1)
 
-  import cats.instances.option.*
+  import cats.instances.option.* // includes functor option
 
   val optionFunctor = Functor[Option]
   val incrementedOption = optionFunctor.map(Option(2))(_ + 1) // Some(3)
 
   import cats.instances.try_.*
 
-  val anIncrementedTry = Functor[Try]().map(Try(42))(_ + 1) // Success(43)
+  val anIncrementedTry = Functor[Try].map(Try(42))(_ + 1) // Some(3)
 
   // generalizing an API
+
   def do10xList(list: List[Int]): List[Int] = list.map(_ * 10)
 
   def do10xOption(option: Option[Int]): Option[Int] = option.map(_ * 10)
 
   def do10xTry(attempt: Try[Int]): Try[Int] = attempt.map(_ * 10)
 
-  // generalize
-  def do10x[F[_] : Functor](container: F[Int]): F[Int] =
-    Functor[F].map(container)(_ * 10)
+  def do10x[F[_]](container: F[Int])(implicit functor: Functor[F]): F[Int] = functor.map(container)(_ * 10)
 
-  // TODO 1: define your own functor for a binary tree
-  // hint: define an object which extends Functor[Tree]
-  sealed trait Tree[+T]
+  trait Tree[+T]
 
-  private object Tree {
-    // "smart" constructors
-    def leaf[T](value: T): Tree[T] = Leaf(value)
+  object Tree {
+    // smart constructors
+    def leaf[T](value: T) = Leaf(value)
 
-    def branch[T](v: T, left: Tree[T], right: Tree[T]): Tree[T] = Branch(v, left, right)
+    def branch[T](value: T, left: Tree[T], right: Tree[T]) = Branch(value, left, right)
   }
 
-  sealed case class Leaf[+T](value: T) extends Tree[T]
+  case class Leaf[+T](value: T) extends Tree[T]
 
-  sealed case class Branch[+T](value: T, left: Tree[T], right: Tree[T]) extends Tree[T]
+  case class Branch[+T](value: T, left: Tree[T], right: Tree[T]) extends Tree[T]
 
-  implicit object TreeFunctor extends Functor[Tree]:
+  implicit object TreeFunctor extends Functor[Tree] {
     override def map[A, B](fa: Tree[A])(f: A => B): Tree[B] = fa match
-      case Leaf(value) => Leaf(f(value))
-      case Branch(value, left, right) =>
-        Branch(f(value), map(left)(f), map(right)(f))
+      case Branch(v, left, right) => Branch(f(v), map(left)(f), map(right)(f))
+      case Leaf(v) => Leaf(f(v))
+  }
 
   // extension method - map
 
   import cats.syntax.functor.*
 
   val tree: Tree[Int] = Tree.branch(40, Tree.branch(5, Tree.leaf(10), Tree.leaf(30)), Tree.leaf(20))
-  val incrementedTree = tree.map(_ * 10)
+  val incrementedTree = tree.map(_ + 1)
 
-  // TODO 2: shorter version of do10x method
-  def do10x2[F[_] : Functor](container: F[Int]): F[Int] =
-    container.map(_ * 10)
+  def do10xShorter[F[_] : Functor](container: F[Int]): F[Int] = container.map(_ * 10)
 
   def main(args: Array[String]): Unit = {
-    //    println(do10x(List(1, 2, 3)))
-    //    println(do10x(Option(2)))
-    //    println(do10x(Try(35)))
-    val tree = Tree.branch(1, Tree.leaf(2), Tree.branch(3, Tree.leaf(4), Tree.leaf(5)))
-    val res = do10x2(tree)
-    println(res)
+    println(do10xShorter(List(1, 2, 3)))
+    println(do10xShorter(Option(2)))
+    println(do10xShorter(Try(35)))
+
+    println(do10xShorter[Tree](Tree.branch(30, Tree.leaf(10), Tree.leaf(20))))
   }
 }
