fe24d7d798b52d8fbc6beaa16e274465f500e8d9
Курчин Иоанн Вячеславович <lvrainman@gmail.com>
2025-09-08 20:22:57 +0300

    Monad part2 recap

--- src/main/scala/part2abstractMath/Monads.scala
+++ src/main/scala/part2abstractMath/Monads.scala
@@ -1,5 +1,9 @@
 package part2abstractMath
 
+import cats.syntax.applicative.*
+import cats.syntax.flatMap.*
+import cats.syntax.functor.*
+
 import java.util.concurrent.Executors
 import scala.concurrent.{ExecutionContext, Future}
 
@@ -47,6 +51,9 @@ object Monads {
     def pure[A](value: A): M[A]
 
     def flatMap[A, B](ma: M[A])(f: A => M[B]): M[B]
+
+    def map[A, B](ma: M[A])(f: A => B): M[B] =
+      flatMap(ma)(x => pure(f(x)))
   }
 
   //Cats monad
@@ -82,10 +89,39 @@ object Monads {
   // generalize
   def getPairs[M[_], A, B](ma: M[A], mb: M[B])(implicit monad: Monad[M]): M[(A, B)] =
     monad.flatMap(ma)(a => monad.map(mb)(b => (a, b)))
-  
+
+  // extension methods  - weirder imports: pure and flatMap
+
+  val oneOption = 1.pure[Option] // implicit Monad[Option] will be used ---> Some(1)
+  val oneList = 1.pure[List] // List(1)
+
+  val oneOptionTransform = oneOption.flatMap(x => (x + 1).pure[Option])
+
+  // monads extend functors
+
+  val oneOptionMapped = Monad[Option].map(Option(2))(x => x + 1)
+  val oneOptionMapped2 = oneOption.map(_ + 2)
+
+  // for-comprehensions
+  val composedOptionFor = for {
+    one <- 1.pure[Option]
+    two <- 2.pure[Option]
+  } yield one + two
+
+  def getPairs2[M[_] : Monad, A, B](ma: M[A], mb: M[B]): M[(A, B)] =
+    for {
+      a <- ma
+      b <- mb
+    } yield (a, b)
+  //    Monad[M].flatMap(ma)(a => Monad[M].map(mb)(b => (a, b)))
+
+  def getPairs3[M[_] : Monad, A, B](ma: M[A], mb: M[B]): M[(A, B)] =
+    ma.flatMap(x => mb.map(y => (x, y)))
+
   def main(args: Array[String]): Unit = {
     println(getPairs(numbersList, charsList))
     println(getPairs(numberOption, charOption))
-    println(getPairs(numberFuture, charFuture)).foreach(println)
+    println(getPairs(numberFuture, charFuture).foreach(println))
+    println(oneOptionTransform)
   }
 }
